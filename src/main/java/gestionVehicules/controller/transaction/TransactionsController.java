8425608f3c3a17c7a01fa23ebc2353240b2e5eb9
priscafehiarisoa <mytyrealy@gmail.com>
2024-02-10 14:30:58 +0300

    n

--- src/main/java/gestionVehicules/controller/transaction/TransactionsController.java
+++ src/main/java/gestionVehicules/controller/transaction/TransactionsController.java
@@ -1,14 +1,14 @@
 package gestionVehicules.controller.transaction;
 
+import gestionVehicules.model.annonce.Annonce;
 import gestionVehicules.model.transactionsBanquaires.Transactions;
 import gestionVehicules.model.user.Utilisateur;
 import gestionVehicules.repository.TransactionsRepository;
+import gestionVehicules.repository.annonce.AnnonceRepository;
+import gestionVehicules.repository.sequence.SequenceRepository;
 import gestionVehicules.repository.user.UtilisateurRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,11 +20,17 @@ public class TransactionsController {
 
     private final TransactionsRepository transactionsRepository;
     private final UtilisateurRepository utilisateurRepository;
+    private final AnnonceRepository annonceRepository;
+    private final SequenceRepository sequenceRepository;
 
     public TransactionsController(TransactionsRepository transactionsRepository,
-                                  UtilisateurRepository utilisateurRepository) {
+                                  UtilisateurRepository utilisateurRepository,
+                                  AnnonceRepository annonceRepository,
+                                  SequenceRepository sequenceRepository) {
         this.transactionsRepository = transactionsRepository;
         this.utilisateurRepository = utilisateurRepository;
+        this.annonceRepository = annonceRepository;
+        this.sequenceRepository = sequenceRepository;
     }
 
     @GetMapping("/getSoldeUtilisateur/{id}")
@@ -64,6 +70,25 @@ public class TransactionsController {
             returningObject.put("statuts",404);
             returningObject.put("erreur","impossible de trouver l'utilisateur");
             return ResponseEntity.ok(returningObject);
+        }
+        return ResponseEntity.ok(returningObject);
+    }
+
+    @PostMapping("/approvisionner")
+    public ResponseEntity<Object> addTransaction(@RequestBody HashMap<String,Object> body){
+        String idUser= String.valueOf(body.get("user"));
+        HashMap<String, Object> returningObject = new HashMap<>();
+
+        double montant= Double.parseDouble(String.valueOf(body.get("montant")));
+        String desctiption = "approvisionnement de compte";
+        try {
+            Annonce annonce= annonceRepository.findAll().get(0);
+            Utilisateur utilisateur= Utilisateur.getOptionalUserById(idUser,utilisateurRepository);
+            Transactions transactions = new Transactions(utilisateur,annonce,1,desctiption,montant,sequenceRepository);
+            transactionsRepository.save(transactions);
+        }catch (Exception e){
+            returningObject.put("statuts",404);
+            returningObject.put("erreur","une erreur est survenue");
         }
         return ResponseEntity.ok(returningObject);
     }
